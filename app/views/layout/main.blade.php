55d6b45f645f90dd4fb4e07e55bf367454141f47
Josh Mountain <joshmountain@gmail.com>
2014-08-20 16:06:59 -0700

    AppUI theme added

--- app/views/layout/main.blade.php
+++ app/views/layout/main.blade.php
@@ -1,111 +1,212 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
+<!--[if IE 9]>         <html class="no-js lt-ie10"> <![endif]-->
+<!--[if gt IE 9]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
 
-    <link rel="icon" href="{{ URL::to('favicon.ico') }}">
+        <title>Item Manager - ManageItems.com</title>
 
-    <title>Item Manager</title>
+        <meta name="description" content="Item Manager is web application built to manage a list of items and assign a status to each item">
+        <meta name="author" content="Josh Mountain">
 
-    <!-- Bootstrap core CSS -->
-    <link href="{{ URL::to('css/bootstrap.min.css') }}" rel="stylesheet">
+        <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1.0">
 
-    <!-- Custom styles for this template -->
-    <link href="{{ URL::to('css/custom.css') }}" rel="stylesheet">
+        <!-- Icons -->
+        <!-- The following icons can be replaced with your own, they are used by desktop and mobile browsers -->
+        <link rel="shortcut icon" href="{{ URL::to('img/favicon.ico') }}">
+        <link rel="apple-touch-icon" href="{{ URL::to('img/icon57.png') }}" sizes="57x57">
+        <link rel="apple-touch-icon" href="{{ URL::to('img/icon72.png') }}" sizes="72x72">
+        <link rel="apple-touch-icon" href="{{ URL::to('img/icon76.png') }}" sizes="76x76">
+        <link rel="apple-touch-icon" href="{{ URL::to('img/icon114.png') }}" sizes="114x114">
+        <link rel="apple-touch-icon" href="{{ URL::to('img/icon120.png') }}" sizes="120x120">
+        <link rel="apple-touch-icon" href="{{ URL::to('img/icon144.png') }}" sizes="144x144">
+        <link rel="apple-touch-icon" href="{{ URL::to('img/icon152.png') }}" sizes="152x152">
+        <!-- END Icons -->
 
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-</head>
+        <!-- Stylesheets -->
+        <!-- Bootstrap is included in its original form, unaltered -->
+        <link rel="stylesheet" href="{{ URL::to('css/bootstrap.min.css') }}">
 
-<body>
-    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-      <div class="container-fluid">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">Item Manager</a>
+        <!-- Related styles of various icon packs and plugins -->
+        <link rel="stylesheet" href="{{ URL::to('css/plugins.css') }}">
+
+        <!-- The main stylesheet of this template. All Bootstrap overwrites are defined in here -->
+        <link rel="stylesheet" href="{{ URL::to('css/main.css') }}">
+
+        <!-- Include a specific file here from css/themes/ folder to alter the default theme of the template -->
+
+        <!-- The themes stylesheet of this template (for using specific theme color in individual elements - must included last) -->
+        <link rel="stylesheet" href="{{ URL::to('css/themes.css') }}">
+        <!-- END Stylesheets -->
+
+        <!-- Modernizr (browser feature detection library) -->
+        <script src="{{ URL::to('js/vendor/modernizr-2.8.3.js') }}"></script>
+    </head>
+    <body>
+        <!-- Page Wrapper -->
+        <!-- In the PHP version you can set the following options from inc/config file -->
+        <!--
+            Available classes:
+
+            'page-loading'      enables page preloader
+        -->
+        <div id="page-wrapper" class="page-loading">
+            <!-- Preloader -->
+            <!-- Preloader functionality (initialized in js/app.js) - pageLoading() -->
+            <!-- Used only if page preloader enabled from inc/config (PHP version) or the class 'page-loading' is added in body element (HTML version) -->
+            <div class="preloader">
+                <div class="inner">
+                    <!-- Animation spinner for all modern browsers -->
+                    <div class="preloader-spinner themed-background hidden-lt-ie10"></div>
+
+                    <!-- Text for IE9 -->
+                    <h3 class="text-primary visible-lt-ie10"><strong>Loading..</strong></h3>
+                </div>
+            </div>
+            <!-- END Preloader -->
+
+            <!-- Page Container -->
+            <!-- In the PHP version you can set the following options from inc/config file -->
+            <!--
+                Available #page-container classes:
+
+                'sidebar-visible-lg-mini'                       main sidebar condensed - Mini Navigation (> 991px)
+                'sidebar-visible-lg-full'                       main sidebar full - Full Navigation (> 991px)
+
+                'sidebar-alt-visible-lg'                        alternative sidebar visible by default (> 991px) (You can add it along with another class)
+
+                'header-fixed-top'                              has to be added only if the class 'navbar-fixed-top' was added on header.navbar
+                'header-fixed-bottom'                           has to be added only if the class 'navbar-fixed-bottom' was added on header.navbar
+            -->
+            <div id="page-container" class="header-fixed-top sidebar-visible-lg-full">
+
+                @include('layout.sidebar')
+
+                <!-- Main Container -->
+                <div id="main-container">
+                    <!-- Header -->
+                    <!-- In the PHP version you can set the following options from inc/config file -->
+                    <!--
+                        Available header.navbar classes:
+
+                        'navbar-default'            for the default light header
+                        'navbar-inverse'            for an alternative dark header
+
+                        'navbar-fixed-top'          for a top fixed header (fixed sidebars with scroll will be auto initialized, functionality can be found in js/app.js - handleSidebar())
+                            'header-fixed-top'      has to be added on #page-container only if the class 'navbar-fixed-top' was added
+
+                        'navbar-fixed-bottom'       for a bottom fixed header (fixed sidebars with scroll will be auto initialized, functionality can be found in js/app.js - handleSidebar()))
+                            'header-fixed-bottom'   has to be added on #page-container only if the class 'navbar-fixed-bottom' was added
+                    -->
+                    <header class="navbar navbar-default navbar-fixed-top">
+                        <!-- Left Header Navigation -->
+                        <ul class="nav navbar-nav-custom">
+                            <!-- Main Sidebar Toggle Button -->
+                            <li>
+                                <a href="javascript:void(0)" onclick="App.sidebar('toggle-sidebar');">
+                                    <i class="fa fa-ellipsis-v fa-fw animation-fadeInRight" id="sidebar-toggle-mini"></i>
+                                    <i class="fa fa-bars fa-fw animation-fadeInRight" id="sidebar-toggle-full"></i>
+                                </a>
+                            </li>
+                            <!-- END Main Sidebar Toggle Button -->
+
+                            <!-- Header Link -->
+                            <li class="hidden-xs animation-fadeInQuick">
+                                <a href=""><strong>Dashboard</strong></a>
+                            </li>
+                            <!-- END Header Link -->
+                        </ul>
+                        <!-- END Left Header Navigation -->
+
+                        @if( Auth::check() )
+                        <!-- Right Header Navigation -->
+                        <ul class="nav navbar-nav-custom pull-right">
+                            <!-- Search Form -->
+                            <li>
+                                <form action="page_ready_search_results.html" method="post" class="navbar-form-custom" role="search">
+                                    <input type="text" id="top-search" name="top-search" class="form-control" placeholder="Search..">
+                                </form>
+                            </li>
+                            <!-- END Search Form -->
+
+                            <!-- User Dropdown -->
+                            <li class="dropdown">
+                                <a href="javascript:void(0)" class="dropdown-toggle" data-toggle="dropdown">
+                                    <img src="http://www.gravatar.com/avatar/{{ md5( strtolower( trim( Auth::user()->email ) ) ) }}?d=http%3A%2F%2Fmanageitems.com%2Fimg%2Favatar.png" alt="avatar">
+                                </a>
+                                <ul class="dropdown-menu dropdown-menu-right">
+                                    <li class="dropdown-header">
+                                        <strong>{{ Auth::user()->username }}</strong>
+                                    </li>
+                                    <li>
+                                        <a href="page_app_email.html">
+                                            <i class="fa fa-inbox fa-fw pull-right"></i>
+                                            Inbox
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="page_app_social.html">
+                                            <i class="fa fa-pencil-square fa-fw pull-right"></i>
+                                            Profile
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="page_app_media.html">
+                                            <i class="fa fa-picture-o fa-fw pull-right"></i>
+                                            Media Manager
+                                        </a>
+                                    </li>
+                                    <li class="divider"><li>
+                                    <li>
+                                        <a href="{{ URL::route('account-change-password') }}">
+                                            <i class="gi gi-lock fa-fw pull-right"></i>
+                                            Change Password
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="{{ URL::route('account-sign-out') }}">
+                                            <i class="fa fa-power-off fa-fw pull-right"></i>
+                                            Sign Out
+                                        </a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <!-- END User Dropdown -->
+                        </ul>
+                        <!-- END Right Header Navigation -->
+                        @endif
+                    </header>
+                    <!-- END Header -->
+
+                    <!-- Page content -->
+                    <div id="page-content">
+
+                        @if(Session::has('global'))
+                            {{ Session::get('global') }}
+                        @endif
+
+                        @yield('content')
+
+                    </div>
+                    <!-- END Page Content -->
+                </div>
+                <!-- END Main Container -->
+            </div>
+            <!-- END Page Container -->
         </div>
-        <div class="navbar-collapse collapse">
-          <ul class="nav navbar-nav navbar-right">
-            @if( Auth::check() )
-              <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-user"></span> {{ Auth::user()->username }} <span class="caret"></span></a>
-                <ul class="dropdown-menu" role="menu">
-                  <li><a href="{{ URL::route('account-change-password') }}">Change Password</a></li>
-                  <li class="divider"></li>
-                  <li><a href="{{ URL::route('account-sign-out') }}">Sign Out</a></li>
-                </ul>
-              </li>
-            @else
-              <li><a href="{{ URL::route('account-sign-in') }}"><span class="glyphicon glyphicon-user"></span> Login</a></li>
-            @endif
-          </ul>
-          @if( Auth::check() )
-            <form class="navbar-form navbar-right">
-              <input type="text" class="form-control" placeholder="Search...">
-            </form>
-          @endif
-        </div>
-      </div>
-    </div>
+        <!-- END Page Wrapper -->
 
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-sm-3 col-md-2 sidebar">
-          @include('layout.sidebar')
-        </div>
-        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+        <!-- Include Jquery library from Google's CDN but if something goes wrong get Jquery from local file (Remove 'http:' if you have SSL) -->
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+        <script>!window.jQuery && document.write(decodeURI('%3Cscript src="js/vendor/jquery-2.1.1.min.js"%3E%3C/script%3E'));</script>
 
-          @if(Session::has('global'))
-            {{ Session::get('global') }}
-          @endif
+        <!-- Bootstrap.js, Jquery plugins and Custom JS code -->
+        <script src="{{ URL::to('js/vendor/bootstrap.min.js') }}"></script>
+        <script src="{{ URL::to('js/plugins.js') }}"></script>
+        <script src="{{ URL::to('js/app.js') }}"></script>
 
-          @yield('content')
-
-          <!--
-          <div class="table-responsive">
-            <table class="table table-striped">
-              <thead>
-                <tr>
-                  <th>#</th>
-                  <th>Header</th>
-                  <th>Header</th>
-                  <th>Header</th>
-                  <th>Header</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>1,001</td>
-                  <td>Lorem</td>
-                  <td>ipsum</td>
-                  <td>dolor</td>
-                  <td>sit</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-          -->
-
-        </div>
-      </div>
-    </div>
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script src="{{ URL::to('js/bootstrap.min.js') }}"></script>
-</body>
-
+        <!-- Load and execute javascript code used only in this page -->
+        <script src="{{ URL::to('js/pages/readyDashboard.js') }}"></script>
+        <script>$(function(){ ReadyDashboard.init(); });</script>
+    </body>
 </html>
--- app/views/layout/sidebar.blade.php
+++ app/views/layout/sidebar.blade.php
@@ -1,27 +1,95 @@
-<ul class="nav nav-sidebar">
-    <li><a href="{{ URL::route('home') }}"><span class="glyphicon glyphicon-home"></span> Home</a></li>
+                <!-- Main Sidebar -->
+                <div id="sidebar">
+                    <!-- Sidebar Brand -->
+                    <div id="sidebar-brand" class="themed-background">
+                        <a href="{{ URL::route('home') }}" class="sidebar-title">
+                            <i class="fa fa-cube"></i> <span class="sidebar-nav-mini-hide">Manage<strong>Items</strong></span>
+                        </a>
+                    </div>
+                    <!-- END Sidebar Brand -->
 
-    @if( Auth::check() )
-        <li><a href=""><span class="glyphicon glyphicon-plus-sign"></span> Add Item</a></li>
-        <li><a href="{{ URL::route('account-sign-out') }}"><span class="glyphicon glyphicon-log-out"></span> Sign Out</a></li>
-    @else
-        <li><a href="{{ URL::route('account-sign-in') }}"><span class="glyphicon glyphicon-log-in"></span> Sign In</a></li>
-        <li><a href="{{ URL::route('account-create') }}"><span class="glyphicon glyphicon-user"></span> Create Account</a></li>
-    @endif
-</ul>
+                    <!-- Wrapper for scrolling functionality -->
+                    <div id="sidebar-scroll">
+                        <!-- Sidebar Content -->
+                        <div class="sidebar-content">
+                            <!-- Sidebar Navigation -->
+                            <ul class="sidebar-nav">
+                                <li>
+                                    <a href="{{ URL::route('home') }}" class=" active"><i class="gi gi-compass sidebar-nav-icon"></i><span class="sidebar-nav-mini-hide">Dashboard</span></a>
+                                </li>
+                                <li class="sidebar-separator">
+                                    <i class="fa fa-ellipsis-h"></i>
+                                </li>
 
-@if( Auth::check() )
+                                @if( Auth::check() )
 
-    <h4>Item Types</h4>
-    <ul class="nav nav-sidebar">
-        <li><a href="{{ URL::route('type-create') }}"><span class="glyphicon glyphicon-plus-sign"></span> Add Item Type</a>
-        @foreach($types as $type)
-            <li><a href="#">{{ $type->label }}</a></li>
-        @endforeach
-    </ul>
+                                <li>
+                                    <a href="#" class="sidebar-nav-menu"><i class="fa fa-chevron-left sidebar-nav-indicator sidebar-nav-mini-hide"></i><i class="fa fa-cube sidebar-nav-icon"></i><span class="sidebar-nav-mini-hide">Items</span></a>
+                                    <ul>
+                                        <li>
+                                            <a href="#">Add New Item</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <a href="#" class="sidebar-nav-menu"><i class="fa fa-chevron-left sidebar-nav-indicator sidebar-nav-mini-hide"></i><i class="gi gi-tags sidebar-nav-icon"></i><span class="sidebar-nav-mini-hide">States</span></a>
+                                    <ul>
+                                        <li>
+                                            <a href="#">Add New State</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <a href="#" class="sidebar-nav-menu"><i class="fa fa-chevron-left sidebar-nav-indicator sidebar-nav-mini-hide"></i><i class="gi gi-more_items sidebar-nav-icon"></i><span class="sidebar-nav-mini-hide">Types</span></a>
+                                    <ul>
+                                        <li>
+                                            <a href="{{ URL::route('type-create') }}">Add New Type</a>
+                                        </li>
+                                    </ul>
+                                </li>
 
-@endif
+                                @else
 
-<p id="copyright">
-    This is <a href="#">Item Manager</a> 1.0.0
-</p>
+                                <li>
+                                    <a href="{{ URL::route('account-sign-in') }}"><i class="gi gi-keys sidebar-nav-icon"></i><span class="sidebar-nav-mini-hide">Sign In</span></a>
+                                </li>
+
+                                <li>
+                                    <a href="{{ URL::route('account-create') }}"><i class="gi gi-plus sidebar-nav-icon"></i><span class="sidebar-nav-mini-hide">Register</span></a>
+                                </li>
+
+                                @endif
+
+                                <li class="sidebar-separator">
+                                    <i class="fa fa-ellipsis-h"></i>
+                                </li>
+
+                                <li>
+                                    <a href="#"><i class="fa fa-support sidebar-nav-icon"></i><span class="sidebar-nav-mini-hide">Support Center</span></a>
+                                </li>
+                            </ul>
+                            <!-- END Sidebar Navigation -->
+                        </div>
+                        <!-- END Sidebar Content -->
+                    </div>
+                    <!-- END Wrapper for scrolling functionality -->
+
+                    <!-- Sidebar Extra Info -->
+                    <div id="sidebar-extra-info" class="sidebar-content sidebar-nav-mini-hide">
+                        <div class="push-bit">
+                            <span class="pull-right">
+                                <a href="javascript:void(0)" class="text-light"><i class="fa fa-plus"></i></a>
+                            </span>
+                            <small><strong class="text-light">780 Items</strong> / 1000 Items</small>
+                        </div>
+                        <div class="progress progress-mini push-bit">
+                            <div class="progress-bar progress-bar-primary" role="progressbar" aria-valuenow="78" aria-valuemin="0" aria-valuemax="100" style="width: 78%"></div>
+                        </div>
+                        <div class="text-center">
+                            <small>Crafted with <i class="fa fa-heart text-danger"></i> by <a href="http://joshmountain.com" target="_blank">Josh Mountain</a></small><br>
+                            <small><span id="year-copy"></span> &copy; Item Manager 1.0.0</small>
+                        </div>
+                    </div>
+                    <!-- END Sidebar Extra Info -->
+                </div>
+                <!-- END Main Sidebar -->

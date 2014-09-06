063d98a61c74049f799412651ae71c79b94a696f
Josh Mountain <joshmountain@gmail.com>
2014-09-05 22:31:46 -0700

    Homepage view updated

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -14,17 +14,23 @@
         </div>
         <!-- END Header -->
     @else
-        <div class="jumbotron">
-              <div class="container">
-                <h1>Welcome to Item Manager!</h1>
-                <p>Item Manager is web application built to manage a list of items and assign a status to each item.</p>
-                <p>Ever wanted to track which Steam games you have completed or which products you are still waiting to ship?</p>
-                <p>Item Manager was designed to be a free, lightweight item tracking tool.</p>
-                <p>
-                    <a class="btn btn-primary btn-lg" role="button" href="{{ URL::route('account-create') }}">Create a Free Account »</a>
-                    <a class="btn btn-default btn-lg" role="button" href="{{ URL::route('account-sign-in') }}">Sign In</a>
-                </p>
-              </div>
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="block">
+                    <div class="jumbotron">
+                        <div class="container">
+                            <h1>Welcome to Item Manager!</h1>
+                            <p>Item Manager is web application built to manage a list of items and assign a status to each item.</p>
+                            <p>Ever wanted to track which Steam games you have completed or which products you are still waiting to ship?</p>
+                            <p>Item Manager was designed to be a free, lightweight item tracking tool.</p>
+                            <p>
+                            <a class="btn btn-primary btn-lg" role="button" href="{{ URL::route('account-create') }}">Create a Free Account »</a>
+                            <a class="btn btn-default btn-lg" role="button" href="{{ URL::route('account-sign-in') }}">Sign In</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </div>
+        </div>
     @endif
 @stop

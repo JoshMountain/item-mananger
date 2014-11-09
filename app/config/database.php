101680bea85dcb83687a186a03c41ae293b8556a
Josh Mountain <joshmountain@gmail.com>
2014-11-09 10:52:27 -0700

    Fixed default db name

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'phxmade',
+			'database'  => 'itemmanager',
 			'username'  => 'homestead',
 			'password'  => 'secret',
 			'charset'   => 'utf8',

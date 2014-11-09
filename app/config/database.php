07f5b78804e7d3ea5155b723a56283f4587a2224
Josh Mountain <joshmountain@gmail.com>
2014-11-09 10:46:00 -0700

    Updated default db name

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'phxproducts',
+			'database'  => 'phxmade',
 			'username'  => 'homestead',
 			'password'  => 'secret',
 			'charset'   => 'utf8',
